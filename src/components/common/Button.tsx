101a8ccff80337bc0e74a40f178c9764210e9a05
Joseph <hujoseph99@gmail.com>
2021-09-25 22:37:25 -0400

    Created button component

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+import { 
+  Button as ChakraButton, 
+  ButtonProps as ChakraButtonProps 
+} from '@chakra-ui/button';
+
+interface ButtonProps extends ChakraButtonProps {
+  text: string;
+}
+
+const Button = ({
+  text = "",
+  ...props
+}: ButtonProps) => (
+  <ChakraButton
+    color="cyan"
+    borderColor="cyan"
+    variant="outline"
+    p={[4, null, 6]}
+    borderRadius={10}
+    fontSize={["sm", null, "md"]}
+    fontWeight="normal"
+    {...props}
+  >
+    {text}
+  </ChakraButton>
+);
+
+export default Button;
