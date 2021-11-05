81b2d133653bccb92dd99cd1c1d3f0f3f159a262
hujoseph99 <hujoseph99@gmail.com>
2021-11-05 00:01:05 -0400

    refactored hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 import { 
-	Flex, 
 	GridItem, 
 	SimpleGrid, 
 	Text, 
@@ -10,97 +9,119 @@ import { useMediaQuery, Box } from '@chakra-ui/react';
 
 import HighlightText from '../common/HighlightText';
 import Button from '../common/Button';
+import SectionContainer from '../common/SectionContainer';
 import { ReactComponent as Avatar } from '../../assets/avatar.svg';
 import { ReactComponent as AvatarBlob } from '../../assets/avatar_blob.svg';
-import Navbar from '../common/navbar/Navbar';
 
 export const Hero = () => {
-	const [hovering, setHovering] = useState(false);
 	const [isLarge] = useMediaQuery('(min-width: 62em)');
+	const [hovering, setHovering] = useState(false);
 
 	return (
-		<Flex bgColor='gray.900' width='100%' height='100vh' flexDir='column' id='hero'>
-			<Navbar />
+		<SectionContainer isPrimary isFullScreen id='hero'>
 			<SimpleGrid
-				columns={[6, null, null, 12]}
-				padding={[10]}
-				flexGrow={1}
-				alignContent='space-between'
+				width='100%'
+				height='100%'
+				columns={[1, null, null, 2]}
 			>
-				<GridItem colSpan={[6, null, null, 12]} />
-				<GridItem colSpan={6}>
-					<Flex width='full' height='full' alignItems='center' justifyContent={isLarge ? 'flex-end' : 'center'}>
-						<Flex
-							flexDirection='column'
-							alignItems='flex-start'
-							maxW='40rem'
-						>
-							<Text fontSize='md'>👋 Hi, my name is</Text>
-							<Text 
-								color='cyan' 
-								fontSize={['5xl', null, null, '7xl']} 
-								fontWeight='extrabold'
-								lineHeight={1}
-								mt={2}
-							>
-								Joseph Hu.
-							</Text>
-							<Text 
-								color='gray.50' 
-								fontSize={['4xl', null, null, '6xl']} 
-								fontWeight='normal'
-								lineHeight={[1.1, null, 1]}
-								mt={4}
-							>
-								I build all things web.
-							</Text>
-							<Text mt={4} fontSize='md'>
-								I'm a software engineer that specializes in web development. 
-								I currently work as a full-stack developer at <HighlightText text='Adeptmind' />.
-							</Text>
-							<Button mt={12} text='Get in Touch' onClick={() => window.open('mailto:hujoseph99@gmail.com')} />
-						</Flex>
-					</Flex>
+				<GridItem colSpan={[1, null, null, 2]} />
+				<GridItem 
+					colSpan={1} 
+					display='flex' 
+					flexDir='column'
+					alignItems='flex-start' 
+					justifyContent='center'
+					maxWidth='40rem'
+				>
+					<Text>👋 Hi, my name is</Text>
+					<Text 
+						color='cyan' 
+						fontSize={['5xl', null, null, '7xl']} 
+						fontWeight='extrabold'
+						lineHeight={1}
+						mt={2}
+					>
+						Joseph Hu.
+					</Text>
+					<Text 
+						color='gray.50' 
+						fontSize={['4xl', null, null, '6xl']} 
+						fontWeight='normal'
+						lineHeight={1}
+						mt={4}
+					>
+						I build all things web.
+					</Text>
+					<Text mt={4}>
+						I'm a software engineer that specializes in web development. 
+						I currently work as a full-stack developer at <HighlightText text='Adeptmind' />.
+					</Text>
+					<Button 
+						mt={12} 
+						text='Get in Touch' 
+						onClick={() => window.open('mailto:hujoseph99@gmail.com')}
+					/>
 				</GridItem>
 				{isLarge && (
-					<GridItem colSpan={6}>
-						<Flex maxW='40rem' height='full' alignItems='flex-start'>
-							<Flex width='full' height='full' alignItems='center' justifyContent='center'>
-								<Box position='absolute' width='30rem' height='30rem' transform='rotate(180deg)'>
-									<AvatarBlob />
-								</Box>
-								<Box position='absolute' width='15rem' height='15rem' mb='3rem'>
-									<Avatar />
-								</Box>
-							</Flex>
-						</Flex>
+					<GridItem 
+						colSpan={1}
+						display='flex'
+						alignItems='center'
+						justifyContent='center'
+					>
+						<Box 
+							position='absolute' 
+							width='100%' 
+							height='100%' 
+							maxWidth='32rem' 
+							maxHeight='32rem' 
+							transform='rotate(180deg)'
+						>
+							<AvatarBlob />
+						</Box>
+						<Box 
+							position='absolute' 
+							width='100%' 
+							height='100%' 
+							maxWidth='15rem' 
+							maxHeight='15rem' 
+							mb='3rem'
+						>
+							<Avatar />
+						</Box>
 					</GridItem>
 				)}
-				<GridItem colSpan={[6, null, null, 12]}>
-					<Flex width='full' height='full' alignItems='flex-end' justifyContent='center'>
+				<GridItem 
+					colSpan={[1, null, null, 2]} 
+					display='flex' 
+					alignItems='flex-end' 
+					justifyContent='center'
+				>
+					<Box 
+						width='2rem' 
+						height='4rem'
+						display='flex'
+						justifyContent='center'
+						borderColor='cyan' 
+						borderWidth={2} 
+						borderRadius={20} 
+						onMouseEnter={() => setHovering(true)}
+						onMouseLeave={() => setHovering(false)}
+					>
 						<Box 
-							width='2rem' 
-							height='4rem'
-							display='flex'
-							justifyContent='center'
-							borderColor='cyan' 
-							borderWidth={2} 
+							className={hovering ? 
+								'animate__animated animate__fadeOutDown animate__infinite animate__slow' :
+								''
+							}
+							width='1rem' 
+							height='1rem' 
+							bgColor='cyan' 
 							borderRadius={20} 
-							onMouseEnter={() => setHovering(true)}
-							onMouseLeave={() => setHovering(false)}
-						>
-							<Box 
-								className={hovering ? 'animate__animated animate__fadeOutDown animate__infinite animate__slow' : ''}
-								width='1rem' 
-								height='1rem' 
-								bgColor='cyan' 
-								borderRadius={20} 
-								marginTop={2}
-							/>
-						</Box>
-					</Flex>
+							marginTop={2}
+						/>
+					</Box>
 				</GridItem>
 			</SimpleGrid>
-		</Flex>
+		</SectionContainer>
 	);
 };
