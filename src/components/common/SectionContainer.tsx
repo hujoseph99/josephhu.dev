c793c7a71bf15219ef99f8785371f105e8ddf6a7
hujoseph99 <hujoseph99@gmail.com>
2021-11-05 00:00:54 -0400

    added section container

---
+++ src/components/common/SectionContainer.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+
+import { Box, Flex } from '@chakra-ui/layout';
+
+interface SectionContainerProps {
+  isPrimary?: boolean;
+  isFullScreen?: boolean;
+  children?: React.ReactNode;
+  id?: string;
+};
+
+const SectionContainer = ({
+  isPrimary = false,
+  isFullScreen = false,
+  children = null,
+  id = ''
+}: SectionContainerProps): JSX.Element => (
+  <Flex
+    width='100%'
+    height={isFullScreen ? '100vh' : 'fit-content'}
+    color={isPrimary ? 'gray.900' : 'gray.800'}
+    alignContent='center'
+    justifyContent='center'
+    paddingX={10}
+    paddingY={10}
+    id={id}
+  >
+    <Box
+      maxWidth='80rem'
+    >
+      {children}
+    </Box>
+  </Flex>
+);
+
+export default SectionContainer;
