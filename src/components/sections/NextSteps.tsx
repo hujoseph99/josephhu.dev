7f2ca9d9c0284b8e49fc9084ed4560103e1c0f3b
hujoseph99 <hujoseph99@gmail.com>
2021-11-11 22:41:23 -0500

    refactored next steps

--- src/components/sections/NextSteps.tsx
+++ src/components/sections/NextSteps.tsx
@@ -1,21 +1,14 @@
-import { Flex, Text } from '@chakra-ui/layout';
 import * as React from 'react';
+
+import { Flex, Text } from '@chakra-ui/layout';
+
 import Button from '../common/Button';
+import SectionContainer from '../common/SectionContainer';
 
 const NextSteps = () => {
   return (
-    <Flex
-      width='100%'
-      height='100vh'
-      flexDirection='column'
-      bgColor="gray.900"
-      paddingY="7rem"
-      justifyContent='center'
-      alignItems='center'
-      paddingX={5}
-      id='contact'
-    >
-      <Flex flexDir='column' maxW='30rem' width='100%' alignItems='center'>
+    <SectionContainer isFullScreen isPrimary id='contact'>
+      <Flex flexDir='column' maxW='35rem' height='100%' alignItems='center' justifyContent='center'>
         <Text 
           fontSize={['2xl', null, null, '3xl']} 
           color='gray.100' 
@@ -26,13 +19,13 @@ const NextSteps = () => {
           What's Next? 
           <Text as='span' color='cyan'> Get in Touch</Text>
         </Text>
-        <Text textAlign='center' mb={10} fontSize='sm'>
+        <Text textAlign='center' mb={10}>
           I’m currently looking for Summer 2022 intership opportunities, so don’t hesitate reach 
           out! Whether you have questions or just want to say hi, I’m always up for a conversation.
         </Text>
         <Button text='Say Hello' onClick={() => window.open('mailto:hujoseph99@gmail.com')} />
       </Flex>
-    </Flex>
+    </SectionContainer>
   )
 }
 
