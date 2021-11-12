7da803e3fe6f158233a82e93d0b78ec8b5ff3ccf
hujoseph99 <hujoseph99@gmail.com>
2021-11-11 21:57:24 -0500

    refactored projects section

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,117 +1,85 @@
 import * as React from 'react';
 
-import { Box, Flex, GridItem, HStack, SimpleGrid, Text, VStack } from '@chakra-ui/layout';
+import { Box, GridItem, GridItemProps, HStack, SimpleGrid, Text } from '@chakra-ui/layout';
 import { useMediaQuery } from '@chakra-ui/media-query';
 import { Image } from '@chakra-ui/image';
 
 import Heading from '../common/Heading';
-import DevRacerIllustration from '../../assets/devracer.png';
-import SudokuIllustration from '../../assets/sudoku.png';
-import HighlightText from '../common/HighlightText';
-import { FaExternalLinkAlt, FaGithub, FaLink, FaShare } from 'react-icons/fa';
-import Bullet from '../common/Bullet';
+import SectionContainer from '../common/SectionContainer';
+import projectData from './ProjectsData';
+import SVGs from '../svgs/SVGs';
 
 const Projects = () => {
   const [isLarge] = useMediaQuery('(min-width: 62em)');
 
   return (
-    <Flex
-      flexDirection='column'
-      width='100%'
-      bgColor="gray.800"
-      justifyContent='center'
-      alignContent='center'
-      paddingY="7rem"
-      overflow='hidden'
-      id='projects'
-    >
+    <SectionContainer isPrimary={false} id='projects'>
       <SimpleGrid 
         width='100%'
         columns={[1, null, null, 2]}
-        padding={[10]}
-        paddingY="5rem"
         spacingX={['1rem', null, null, '3rem', '6rem']}
       >
-        <GridItem colSpan={1} display='flex' justifyContent='flex-end'>
-          <Box width='100%' maxWidth='30rem'>
-            <Heading text="My Projects"  />
-          </Box>
+        <GridItem colSpan={1} display='flex' justifyContent='flex-end' maxWidth='35rem'>
+          <Heading text="My Projects"  />
         </GridItem>
-        <GridItem colSpan={1} />
-        {isLarge && 
-          <GridItem colSpan={1} display='flex' justifyContent='flex-end' mb={20} mt={10}>
-            <Box width='100%' maxWidth='30rem'>
-              <Image src={DevRacerIllustration} borderRadius={10} />
-            </Box>
-          </GridItem>
-        }
-        <GridItem colSpan={1} display='flex' alignItems='center' mb={20} mt={10}>
-          <Flex flexDirection='column' width='100%' maxWidth='30rem' alignItems='flex-start'>
-            <Text fontSize='xs' color='cyan'>Feature Project</Text>
-            <Text fontSize='2xl' color='gray.100' fontWeight='bold' mb={2}>DevRacer</Text>
-            <Text mb={1}>
-              An online <HighlightText text="multiplayer" /> typing game inspired by Typeracer made 
-              for developers. Type popular code snippets from different languages and race against 
-              your friends!
-            </Text>
-            <Text mb={2}>
-              This is a <HighlightText text="full-stack" /> web app that is built on top of 
-              <HighlightText text=" React" />, <HighlightText text="Go" />, and 
-              <HighlightText text=" MongoDB" />. It also incorporates various other technologies 
-              such as <HighlightText text="OAuth" />, <HighlightText text="JWT" />, 
-              <HighlightText text=" GraphQL" />, <HighlightText text="Redux" />, and 
-            <HighlightText text=" Websockets" />.
-            </Text>
-            <HStack mt="1rem">
-              <FaGithub color="#A0AEC0" />
-              <FaExternalLinkAlt color="#A0AEC0" />
-            </HStack>
-          </Flex>
-        </GridItem>
-        <GridItem colSpan={1} display='flex' alignItems='center' justifyContent='flex-end'>
-          <Flex flexDirection='column' width='100%' maxWidth='30rem' mb={20} >
-            <Text fontSize='xs' color='cyan'>Feature Project</Text>
-            <Text fontSize='2xl' color='gray.100' fontWeight='bold' mb={2}>Sudoku</Text>
-            <Text mb={1}>
-              A simple sudoku web app that served as my introductory project to web development. 
-              It is a completely frontend app which is built on <HighlightText text="React" /> and 
-              <HighlightText text=" Bootstrap" />. The game 
-              itself supports various features such as:
-            </Text>
-            <SimpleGrid
-              columns={[1, null, null, 2]}
-              spacingY={1}
-              width='full'
-              mb={2}
-            >
-              <GridItem>
-                <Bullet text="Note Taking" />
+        {isLarge && <GridItem colSpan={1} />}
+        {
+          projectData.map((data, idx) => {
+            const image = isLarge && (
+              <GridItem colSpan={1} display='flex' justifyContent='center' alignItems='center' mt={16}>
+                <Box width='100%' maxWidth='35rem'>
+                  <Image src={data.image} borderRadius={10} />
+                </Box>
               </GridItem>
-              <GridItem>
-                <Bullet text="Custom difficulties" />
+            );
+
+            const mobileCardProps : GridItemProps = !isLarge ? {
+              borderRadius: 10,
+              boxShadow: '1px 5px 12px rgba(0, 0, 0, 0.4)',
+              padding: '2rem', 
+              backgroundColor: '#171d28'
+            } : {};
+
+            const content = (
+              <GridItem 
+                colSpan={1} 
+                display='flex' 
+                flexDirection='column'
+                alignItems='flex-start' 
+                justifyContent='center'
+                maxWidth='35rem'
+                mt={16} 
+                {...mobileCardProps}
+              >
+                <Text fontSize='xs' color='cyan'>Feature Project</Text>
+                <Text fontSize='2xl' color='gray.100' fontWeight='bold' mb={2}>{data.title}</Text>
+                {data.body.map(section => (
+                  <Box mb={3} width='100%'>
+                    {section}
+                  </Box>
+                ))}
+                <HStack spacing={4} mt={2}>
+                  {data.links.map(link => (
+                    <Box 
+                      _hover={{ color: 'cyan' }} 
+                      color='gray.400' 
+                      onClick={() => window.open(link.link)}
+                      css={{
+                        cursor: 'pointer'
+                      }}
+                    >
+                      <SVGs name={link.icon} width='1.5rem' height='1.5rem' />
+                    </Box>
+                  ))}
+                </HStack>
               </GridItem>
-              <GridItem>
-                <Bullet text="Hints" />
-              </GridItem>
-              <GridItem>
-                <Bullet text="Solver" />
-              </GridItem>
-            </SimpleGrid>
-            <HStack mt="1rem">
-              <FaGithub color="#A0AEC0" />
-              <FaExternalLinkAlt color="#A0AEC0" />
-            </HStack>
-          </Flex>
-        </GridItem>
-        {isLarge && 
-          <GridItem colSpan={1} display='flex' justifyContent='flex-start'>
-            <Box width='100%' maxWidth='30rem'>
-              <Image src={SudokuIllustration} borderRadius={10} />
-            </Box>
-          </GridItem>
+            );
+
+            return idx % 2 === 0 ? [image, content] : [content, image];
+          })
         }
       </SimpleGrid>
-    </Flex>
+    </SectionContainer>
   );
 };
 
---
+++ src/components/sections/ProjectsData.tsx
@@ -0,0 +1,121 @@
+import * as React from 'react';
+
+import { GridItem, SimpleGrid, Text } from '@chakra-ui/layout';
+
+import HighlightText from '../common/HighlightText';
+import DevRacerIllustration from '../../assets/devracer.png';
+import SudokuIllustration from '../../assets/sudoku.png';
+import Bullet from '../common/Bullet';
+import { SVGNameType } from '../svgs/SVGs';
+
+interface LinksType {
+  icon: SVGNameType;
+  link: string;
+}
+
+interface ProjectDataType {
+  title: string;
+  body: React.ReactNode[];
+  links: LinksType[];
+  image: string;
+};
+
+const projectData: ProjectDataType[] = [
+  {
+    title: 'DevRacer',
+    body: [
+      <Text>
+        An online <HighlightText text="multiplayer" /> typing game inspired by Typeracer made 
+        for developers. Type popular code snippets from different languages and race against 
+        your friends!
+      </Text>,
+      <Text>
+        This is a <HighlightText text="full-stack" /> web app that is built on top of 
+        <HighlightText text=" React" />, <HighlightText text="Go" />, and 
+        <HighlightText text=" MongoDB" />. It also incorporates various other technologies 
+        such as <HighlightText text="OAuth" />, <HighlightText text="JWT" />, 
+        <HighlightText text=" GraphQL" />, <HighlightText text="Redux" />, and 
+        <HighlightText text=" Websockets" />.
+      </Text>
+    ],
+    links: [
+      {
+        icon: 'github',
+        link: 'https://github.com/hujoseph99/devracer'
+      },
+      {
+        icon: 'external',
+        link: 'https://hujoseph99.github.io/devracer/#/'
+      }
+    ],
+    image: DevRacerIllustration
+  },
+  {
+    title: 'Sudoku',
+    body: [
+      <Text>
+        A simple sudoku web app that served as my introductory project to web development. 
+        It is a completely frontend app which is built on <HighlightText text="React" /> and 
+        <HighlightText text=" Bootstrap" />. The game 
+        itself supports various features such as:
+      </Text>,
+      <SimpleGrid
+        columns={[1, 2]}
+        spacingY={1}
+      >
+        <GridItem><Bullet text="Note Taking" /></GridItem>
+        <GridItem><Bullet text="Custom difficulties" /></GridItem>
+        <GridItem><Bullet text="Hints" /></GridItem>
+        <GridItem><Bullet text="Solver" /></GridItem>
+      </SimpleGrid>
+    ],
+    links: [
+      {
+        icon: 'github',
+        link: 'https://github.com/hujoseph99/sudoku'
+      },
+      {
+        icon: 'external',
+        link: 'https://hujoseph99.github.io/sudoku/'
+      }
+    ],
+    image: SudokuIllustration
+  },
+  {
+    title: 'Sistering Scheduler',
+    body: [
+      <Text>
+        I am currently working as a <HighlightText text='senior developer' /> on the
+        <HighlightText text=' Sistering' /> team at <HighlightText text='UW Blueprint' /> — a 
+        student-run initiative to help build tech solutions for non-profits. I will be leveraging my
+        experience to mentor juniors and influence the direction of the product.
+      </Text>,
+      <Text>
+        This will be a <HighlightText text='full-stack web app' /> incorporating:
+      </Text>,
+      <SimpleGrid
+        columns={[1, 2]}
+        spacingY={1}
+      >
+        <GridItem><Bullet text="React" /></GridItem>
+        <GridItem><Bullet text="Node" /></GridItem>
+        <GridItem><Bullet text="Typescript" /></GridItem>
+        <GridItem><Bullet text="PostgreSQL" /></GridItem>
+        <GridItem><Bullet text="GraphQL" /></GridItem>
+        <GridItem><Bullet text="Firebase" /></GridItem>
+      </SimpleGrid>
+    ],
+    links: [
+      {
+        icon: 'github',
+        link: 'https://github.com/uwblueprint/sistering'
+      },
+    ],
+    image: SudokuIllustration
+  },
+  
+];
+
+export default projectData;
+
+
